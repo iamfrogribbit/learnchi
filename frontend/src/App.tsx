b7dc1d305e10c4b8d931d7acc6fbc0bfbb8b0632
Riley <rileyguy665@hotmail.com>
2026-08-13 08:46:30 +0800

    added .env to .gitignore and fixed routing issue

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import AppNav from './components/App/AppNav.tsx';
 import { Outlet } from 'react-router';
-import axios from 'axios'
 
 function App() {
 
+    const url = import.meta.env.VITE_API_URL;
+    console.log(url)
+
     async function getData() {
         try {
-            const response = await axios.get(import.meta.env.VITE_API_URL);
+            const response = await fetch(url);
             console.log(response)
         } catch (error) {
             console.log('error occurred:' + error);
